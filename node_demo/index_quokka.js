406a53b014a6bd9b33b1026879230e2dc62f207a
semrok@gmail.com <semrok@gmail.com>
2018-11-27 06:02:32 +0100

    nodejs demo

---
+++ node_demo/index_quokka.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+
+let data = [[45, 98, 41, 65, 33, [99, 42]], [745, 46, 12], [78, 45, [32, 33]]];
+
+let even = [];
+
+for (const number of data) {
+  if (number % 2 == 0) {
+    even.push(number);
+  }
+}
+even;
